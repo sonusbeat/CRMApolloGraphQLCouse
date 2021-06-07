6dc42195c9c036c64e79d127540180d94bab49fb
Daniel <sonusbeat@gmail.com>
2021-06-06 19:41:38 -0500

    Get Types and Resolvers

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { ApolloServer, gql } = require('apollo-server');
+
+// Schema
+const typeDefs = gql`
+
+    type Course {
+        title: String
+        technology: String
+    }
+
+    type Query {
+        getCourses : [Course]
+    }
+
+`;
+
+const courses = [
+    {
+        title: 'JavaScript Definitive Modem Guide, Build +10 Projects',
+        technology: 'JavaScript ES6',
+    },
+    {
+        title: 'React – The Complete Guide: Hooks Context Redux MERN +15 Apps',
+        technology: 'React',
+    },
+    {
+        title: 'Node.js – Bootcamp Development Web, inc MVC and REST API’s',
+        technology: 'Node.js'
+    }, 
+    {
+        title: 'ReactJS Advanced – FullStack React GraphQL and Apollo',
+        technology: 'React'
+    }
+];
+
+// Resolvers
+const resolvers = {
+    Query: {
+        getCourses: () => courses
+    }
+};
 
 // Create a new ApolloServer instance
-const server = new ApolloServer();
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
 
 // Run Server
 server.listen().then( ({ url }) => {
