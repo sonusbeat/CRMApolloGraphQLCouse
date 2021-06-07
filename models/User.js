4e1ead87fff64ad8c46df830d39efc8b90a01f8e
Daniel <sonusbeat@gmail.com>
2021-06-06 23:49:14 -0500

    Creating User Model

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const UsersSchema = mongoose.Schema({
+
+    first_name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    last_name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+
+    created_at: {
+        type: Date,
+        default: Date.now(),
+    }
+
+});
+
+module.exports = mongoose.model("User", UsersSchema);
