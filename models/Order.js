6ddc417847fa579f0564f167721bba85ab6f79e8
Daniel <sonusbeat@gmail.com>
2021-06-08 22:36:41 -0500

    Order Model

---
+++ models/Order.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const OrdersSchema = mongoose.Schema({
+
+  order: {
+    type: Array,
+    required: true,
+  },
+
+  total: {
+    type: Number,
+    required: true,
+  },
+
+  client: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "Client",
+  },
+
+  seller: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+
+  status: {
+    type: string,
+    default: "PENDING",
+  },
+
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+
+});
+
+module.exports = mongoose.model("Order", OrdersSchema);
